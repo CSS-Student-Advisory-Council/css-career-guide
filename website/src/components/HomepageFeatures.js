c3b0b98b5031e4ff20884a9385da6cadb5bf35db
molochxte <54558959+molochxte@users.noreply.github.com>
2022-06-01 20:51:15 -0700

    modified homepage

--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -4,32 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Curated for CSS Students',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default, // image removed in HomePageFeatures()
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This guide shares perspectives from CSS students on preparing for internships, as well as student clubs and resources (on and off-campus) to check out!
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Hosted by the Student Advisory Council and Computing Careers Club',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+               The <a href="https://csssac.org/">Student Advisory Council</a> is a student organization focused on student curricular feedback and connecting with faculty to improve the CSS curriculum and program. In Spring 2022, the <a href="https://cscareersuwb.netlify.app/">Computing Careers Club</a> was formed to encompass SAC's student-facing projects, including managing this guide. 
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Open to Contributions',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        We are currently accepting contributions on our <a href="https://github.com/CSS-Student-Advisory-Council/css-career-guide"> GitHub </a>. Check out or create an issue about topics to expand on in the guide!
       </>
     ),
   },
@@ -38,9 +35,9 @@ const FeatureList = [
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
+{/*       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
-      </div>
+      </div> */}
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
